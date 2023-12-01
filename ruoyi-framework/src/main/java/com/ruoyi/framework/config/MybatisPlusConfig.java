277c2c095b9ee6bceec509cd6cf79142af61d428
dongth <12551773+tlfdong@user.noreply.gitee.com>
2023-12-01 08:32:55 +0800

    集成mybatisplus实现mybatis增强

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/MybatisPlusConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/MybatisPlusConfig.java
@@ -43,12 +43,13 @@ public class MybatisPlusConfig
         // 设置数据库类型为mysql
         paginationInnerInterceptor.setDbType(DbType.MYSQL);
         // 设置最大单页限制数量，默认 500 条，-1 不受限制
+
         paginationInnerInterceptor.setMaxLimit(-1L);
         return paginationInnerInterceptor;
     }
 
     /**
-     * 乐观锁插件
+     * 乐观锁插件 https://baomidou.com/guide/interceptor-optimistic-locker.html
      */
     public OptimisticLockerInnerInterceptor optimisticLockerInnerInterceptor()
     {
@@ -70,6 +71,7 @@ public class MybatisPlusConfig
     public MetaObjectHandler metaObjectHandler() {
         return new CreateAndUpdateMetaObjectHandler();
     }
+
 
     /**
      * PaginationInnerInterceptor 分页插件，自动识别数据库类型
